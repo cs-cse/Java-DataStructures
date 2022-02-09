2cb93e5d542dc2c0f0c517b6cc75a747fb4d3673
Chaitanya <chaitanyaa.shrivastava@gmail.com>
2022-02-09 10:31:58 +0530

    Binary search tree methods

--- dsa-BinaryTree/src/BinarySearchTree.java
+++ dsa-BinaryTree/src/BinarySearchTree.java
@@ -1,6 +1,7 @@
 public class BinarySearchTree {
     private TreeNode root;
-    private class TreeNode{
+
+    private class TreeNode {
         private TreeNode left;
         private TreeNode right;
         private int data;
@@ -9,6 +10,49 @@ public class BinarySearchTree {
             this.data = data;
         }
     }
+
+    public TreeNode insert(TreeNode root, int value) {
+//  method to insert a node in Binary search tree.
+        if (root == null) {
+            root = new TreeNode(value);
+            return root;
+        }
+        if (value < root.data) {
+            root.left = insert(root.left, value);
+        } else {
+            root.right = insert(root.right, value);
+        }
+        return root;
+    }
+    public TreeNode search(TreeNode root, int key) {
+//  method to search a given key in a Binary search tree.
+        if (root == null) {
+            return root;
+        }
+        if (key < root.data) {
+            return search(root.left, key);
+        } else {
+            return search(root.right, key);
+        }
+
+    }
+    public boolean isValid(TreeNode root, long min, long max) {
+//  method to check the validity of a Binary search tree.
+        if (root == null) {
+            return true;
+        }
+        if (root.data <= min || root.data >= max) {
+            return false;
+        }
+        boolean left = isValid(root.left, root.data, max);
+            if (left){
+             boolean right=isValid(root.right,root.data,max);
+             return right;
+            }
+        return false;
+    }
+
+
 
 
     public static void main(String[] args) {
