7b288308aa58e359d2d56727cf7c1cffc18b4d67
Chaitanya <chaitanyaa.shrivastava@gmail.com>
2022-02-12 12:35:12 +0530

    New heap methods added

--- dsa-Heap/src/MaxPriorityQueue.java
+++ dsa-Heap/src/MaxPriorityQueue.java
@@ -12,37 +12,74 @@ public class MaxPriorityQueue {
     public int size(){
         return n;
     }
-    public void insert(int x){
-//  method to add an element in a priority queue
-        if(n==heap.length-1){
-            resize(2*heap.length);
+//    ##  Bottom up Reheapify  ##
+            public void insert(int x){
+        //  method to add an element in a priority queue
+                if(n==heap.length-1){
+                    resize(2*heap.length);
+                }
+                n++;
+                heap[n]=x;
+                swim(x);
+            }
+            public void swim(int k){
+                while (k>1 && heap[k/2]<heap[k]){
+                    int temp=heap[k];
+                    heap[k]=heap[k/2];
+                    heap[k/2]=temp;
+                    k=k/2;
+                }
+            }
+        public void resize(int capacity){
+        //  method to resize the array to increase the capacity of the heap
+                Integer[]temp=new Integer[capacity];
+                for(int i=0;i<heap.length;i++){
+                    temp[i]=heap[i];
+                }
+                heap=temp;
+            }
+//        Top down Reheapify
+        public int deleteMax(){
+        int max=heap[1];
+        swap(1,n);
+        n--;
+        sink(1);
+        heap[n+1]=null;
+        if(n>0&& (n==(heap.length-1)/4)){
+            resize(heap.length/2);
         }
-        n++;
-        heap[n]=x;
-        swim(x);
-    }
-    public void swim(int k){
-        while (k>1 && heap[k/2]<heap[k]){
-            int temp=heap[k];
-            heap[k]=heap[k/2];
-            heap[k/2]=temp;
-            k=k/2;
+        return max;
         }
-    }
-public void resize(int capacity){
-//  method to resize the array to increase the capacity of the heap
-        Integer[]temp=new Integer[capacity];
-        for(int i=0;i<heap.length;i++){
-            temp[i]=heap[i];
+        public void sink(int k){
+        while (2*k<=n){
+            int j=2*k;
+            if(j<n&&heap[j]<heap[j+1]){
+                j++;
+            }
+            if(heap[k]>=heap[j]){
+                break;
+            }
+            swap(k,j);
+            k=j;
+            }
         }
-        heap=temp;
+        public void swap(int a, int b){
+        int temp=heap[a];
+        heap[a]=heap[b];
+        heap[b]=temp;
+        }
 
-
-}
     public static void main(String[] args) {
         MaxPriorityQueue pq=new MaxPriorityQueue(3);
         System.out.println(pq.size());
         System.out.println(pq.isEmpty());
+        pq.insert(4);
+        pq.insert(5);
+        pq.insert(6);
+        pq.insert(1);
+        pq.insert(3);
+        System.out.println(pq.size());
+
 
     }
 }
