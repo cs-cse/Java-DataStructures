21cb584c5996f5856c2f013eed31fc32eeba1b88
Chaitanya <chaitanyaa.shrivastava@gmail.com>
2022-02-16 09:27:03 +0530

    Graph using list and bfs added

--- dsa-Graph/src/UndirectedGraph.java
+++ dsa-Graph/src/UndirectedGraph.java
@@ -1,13 +1,28 @@
 public class UndirectedGraph {
 //  Implementing Undirected graph using adjacency matrix.
-    int [][]adjMatrix;
-
+    private int [][]adjMatrix;
+    private int v;
+    private int e;
     public UndirectedGraph(int nodes) {
+        this.v=nodes;
+        this.e=0;
         this.adjMatrix = new int[nodes][nodes];
     }
     public void addEdge(int u,int v){
         this.adjMatrix[u][v]=1;
         this.adjMatrix[v][u]=1;
+    }
+    public String toString(){
+        StringBuilder sb=new StringBuilder();
+        sb.append(v+" vertices"+ e + " edges"+"\n");
+        for(int a=0;a<v;a++){
+            sb.append(a+": ");
+            for(int w:adjMatrix[v]){
+                sb.append(w+" ");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
     }
 
     public static void main(String[] args) {
@@ -16,5 +31,6 @@ public class UndirectedGraph {
         g.addEdge(1,2);
         g.addEdge(2,3);
         g.addEdge(3,0);
+        System.out.println(g);
     }
 }
